2b9881ce6c10f1df07aaacb6c55b53db3122b027
eileen <zwleileen@gmail.com>
2025-01-21 14:22:30 +0800

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,61 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { show } from "./services/weatherService";
-import { useEffect } from "react";
 //? Copied the using fetch from MDN
 
 //? call the function -> check the network tab
 
-const App = () => {
-  const [weather, setWeather] = useState("???");
+//? React -> App -> show() -> setWeather()
+//? pure -> no side effect
+const addTen = (x) => x + 10;
+console.log(addTen(7));
 
-  useEffect(() => {
-    const fetchTaipei = async() => {
-      const taipei = await show('Taipei')
-        setWeather(taipei.current.temp_c);
+const App = () => {
+  const [weather, setWeather] = useState({});
+  const [city, setCity] = useState("Seattle");
+  const [loading, setLoading] = useState("");
+
+  const A = () => {
+    const fetchCity = async () => {
+      const cityWeather = await show(city);
+      console.log(cityWeather);
+      setWeather(cityWeather);
     };
-      fetchTaipei();
-    }, []);
+    fetchCity();
+  };
+  //? function (not ASYNC) to run
+  //? array of things that when you change you want the function to run again
+  useEffect(A, [city]);
+
+  // const taipyei = show("Taipei");
+  // console.log(taipei);
 
   const handleClick = async () => {
+    //?
+    setLoading("loading...");
     const data = await show("London");
-    console.log("data", data);
-    setWeather(data.current.temp_c);
+    // console.log("data", data);
+    setWeather(data);
+    setLoading("");
+    // setCity(count + 1);
   };
+
   return (
     <>
-      <p>Weather:{weather} </p>
+      <label>
+        City:
+        <select
+          name="city"
+          value={city}
+          onChange={(event) => setCity(event.target.value)}
+        >
+          <option>Singapore</option>
+          <option>London</option>
+          <option>Seoul</option>
+        </select>
+      </label>
+      <p>Weather: {weather?.current?.humidity}</p>
       <button onClick={handleClick}>Click</button>
+      <p>{loading}</p>
     </>
   );
 };
