a4c031e9e24664dceb7568fccbd0c5080f4a3692
eileen <zwleileen@gmail.com>
2025-01-21 11:46:32 +0800

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import { useState } from "react";
 import { show } from "./services/weatherService";
+import { useEffect } from "react";
 //? Copied the using fetch from MDN
 
 //? call the function -> check the network tab
 
 const App = () => {
   const [weather, setWeather] = useState("???");
+
+  useEffect(() => {
+    const fetchTaipei = async() => {
+      const taipei = await show('Taipei')
+        setWeather(taipei.current.temp_c);
+    };
+      fetchTaipei();
+    }, []);
 
   const handleClick = async () => {
     const data = await show("London");
